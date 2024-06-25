515640da44b099818d2a2427bace8ba095b93837
Khanh Quang <khanhquang213@gmail.com>
2024-06-25 17:18:13 +0700

    update login signup Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,10 @@ import LoginSignup from './Pages/LoginSignup';
 import Shop from './Pages/Shop';
 import Cart from './Pages/Cart';
 import Product from './Pages/Product';
+import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png';
+import women_banner from './Components/Assets/banner_women.png';
+import kid_banner from './Components/Assets/banner_kids.png';
 
 function App() {
 	return (
@@ -14,15 +18,16 @@ function App() {
 				<Navbar />
 				<Routes>
 					<Route path="/" element={<Shop />} />
-					<Route path="/mens" element={<ShopCategory category="men" />} />
-					<Route path="/womens" element={<ShopCategory category="women" />} />
-					<Route path="/kids" element={<ShopCategory category="kid" />} />
+					<Route path="/mens" element={<ShopCategory banner={men_banner} category="men" />} />
+					<Route path="/womens" element={<ShopCategory banner={women_banner} category="women" />} />
+					<Route path="/kids" element={<ShopCategory banner={kid_banner} category="kid" />} />
 					<Route path="/product" element={<Product />}>
 						<Route path=":productId" element={<Product />} />
 					</Route>
 					<Route path="/cart" element={<Cart />} />
 					<Route path="/login" element={<LoginSignup />} />
 				</Routes>
+				<Footer />
 			</BrowserRouter>
 		</div>
 	);
